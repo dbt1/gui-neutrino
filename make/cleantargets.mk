2e029b9e98e771c11598814d9d71db0d3fa1bcbc
Thilo Graf <dbt@novatux.de>
2015-01-05 23:08:02 +0100

    cleantargets.mk: add target deps-clean

--- make/cleantargets.mk
+++ make/cleantargets.mk
@@ -2,16 +2,19 @@
 clean:
 	make neutrino-clean
 
+deps-clean:
+	rm -fv $(DEPDIR)/*
+
 # rebuild all except the toolchain
-rebuild-clean: clean
+rebuild-clean: clean deps-clean
 	-rm -rf $(TARGETPREFIX)
 	-rm -rf $(HOSTPREFIX)
 	-set -e;cd $(BUILD_TMP); \
 		find . -mindepth 1 -maxdepth 1 ! -name 'crosstool*' -print0 | \
 		xargs --no-run-if-empty -0 rm -rf
-	-rm $(DEPDIR)/*
 
 all-clean: rebuild-clean
 	-rm -rf $(CROSS_BASE)
+
 
 PHONY += clean rebuild-clean all-clean
